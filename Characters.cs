422f287054eef0594b0363310fef464dd0066bee
luketerry147 <luketerry147@gmail.com>
2016-07-13 14:40:10 -0700

    Create Characters.cs

---
+++ Characters.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Character
+{
+	public Character()
+	{
+        //Attributes
+        int Strength, Dexterity, Constitution, Intelligence, Wisdom, Charisma;
+        //Related Data
+        int Hitpoints;
+        int level;
+        //Type
+        Boolean monstrous = false;
+        //Position
+        int x, y, z;
+	}
+}
