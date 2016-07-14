24b7a1830d5bde27b3a8ff194259856548ac6aeb
luketerry147 <luketerry147@gmail.com>
2016-07-14 01:05:01 -0700

    Add files via upload

---
+++ Profession.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Profession
+{
+    //Attributes
+    String name;
+    int hitDie;
+    Skill[] skills;
+    int level;
+	public Profession(String iscalled, Skill[] isprofat, int hashitdie)
+	{
+        name = iscalled;
+        int numskills = isprofat.Length;
+        skills = new Skill[numskills];
+        for(int i = 0; i < numskills; i++)
+        {
+            skills[i] = isprofat[i];
+        }
+	}
+
+    public void setLevel(int newlevel)
+    {
+        if(newlevel > 0)
+        {
+            this.level = newlevel;
+        }        
+    }
+
+    public Skill[] getSkillProfs()
+    {
+        return skills;
+    }
+
+    public int getHitDie()
+    {
+        return hitDie;
+    }
+}
---
+++ Status.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Status
+{
+    String name;
+    //Need to add variable amount of effects
+	public Status(String what)
+	{
+        name = what;
+	}
+}
